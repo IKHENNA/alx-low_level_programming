ab2650ad317bfe25c00ede3d31b14ac4e3228e3c
ikhenna <ikhennamathias@gmail.com>
2022-09-13 17:09:42 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,39 @@
 #include "main.h"
+
+
+
 /**
- * main -Entry point
  *
- * return: always 0 (sucess)
+ *  * main - main entry
  *
- */
+ *   * Return: int
+ *
+ *   */
+
+
+
 int main(void)
+
 {
-_putchar("_")
-_putchar("p")
-_putchar("u")
-_putchar("t")
-_putchar("c")
-_putchar("h")
-_putchar("a")
-_putchar("r")
-_putchar("\n")
-return (0);
+
+		_putchar('_');
+
+			_putchar('p');
+
+				_putchar('u');
+
+					_putchar('t');
+
+						_putchar('c');
+
+							_putchar('h');
+
+								_putchar('a');
+
+									_putchar('r');
+
+										_putchar(10);
+
+											return (0);
+
 }
