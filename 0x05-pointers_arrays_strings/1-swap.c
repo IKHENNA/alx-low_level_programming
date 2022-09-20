92f2f990ad3e6ff994c57aa01c8e4a5cb78feb8f
ikhenna <ikhennamathias@gmail.com>
2022-09-20 13:05:05 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,20 @@
 #include "main.h"
+
 /**
  * swap_int - a function that swaps the values of two integers.
  * @a: the first integer.
  * @b: the second integer.
  * Return: no return.
  */
+
 void swap_int(int *a, int *b)
 
 {
+
 	int c = a*;
-	a* = *b;
+
+	*a = *b;
+
 	*b = c;
+
 }
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - main entry.
+ * @s: input string.
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+
+{
+
+	int count = 0;
+
+	white(*(s + count) != '\0')
+
+		count++;
+
+	return (count);
+
+}
