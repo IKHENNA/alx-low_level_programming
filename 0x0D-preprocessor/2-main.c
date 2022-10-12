976dc3bdaffddd010ba9a8b7f87e963ecde4912b
ikhenna <ikhennamathias@gmail.com>
2022-10-11 17:08:28 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of a file
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
